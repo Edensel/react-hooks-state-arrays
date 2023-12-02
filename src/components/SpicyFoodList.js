254bdc1041e27575cdfe383dc8e54679bc51286f
Esekon <densel.esekon@moringaschool.com>
2023-12-02 23:58:18 +0300

    Completed Assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,17 +1,60 @@
 import React, { useState } from "react";
-import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
-function SpicyFoodList() {
-  const [foods, setFoods] = useState(spicyFoods);
+const SpiceFoodList = () => {
+  // Sample spicyFoods array for initial data
+  const spicyFoodsData = [
+    { id: 1, name: 'Spicy Chicken', heatLevel: 4, cuisine: 'American' },
+    { id: 2, name: 'Sichuan Hotpot', heatLevel: 5, cuisine: 'Sichuan' },
+    // Add more sample data as needed...
+  ];
 
-  function handleAddFood() {
-    const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
-  }
+  // State to hold the list of spicy foods
+  const [foods, setFoods] = useState(spicyFoodsData);
 
+  // Function to generate new spicy food
+  const getNewSpicyFood = () => {
+    // Generate a new food object (for demonstration purposes)
+    const newFood = {
+      id: foods.length + 1,
+      name: `New Spicy Food ${foods.length + 1}`,
+      heatLevel: Math.floor(Math.random() * 5) + 1,
+      cuisine: 'Random Cuisine', // You can adjust this based on your needs
+    };
+    return newFood;
+  };
+
+  // Handler to add new spicy food
+  const handleAddFood = () => {
+    const newFood = getNewSpicyFood();
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
+  };
+
+  // Handler to remove spicy food by ID
+  const handleLiClick = (id) => {
+    const newFoodArray = foods.filter((food) => food.id !== id);
+    setFoods(newFoodArray);
+  };
+
+  // Handler to update heat level of spicy food by ID
+  const handleUpdateHeatLevel = (id) => {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      }
+      return food;
+    });
+    setFoods(newFoodArray);
+  };
+
+  // JSX for rendering the list and button
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+      <button onClick={() => handleUpdateHeatLevel(food.id)}>+1 Heat</button>
     </li>
   ));
 
@@ -21,6 +64,7 @@ function SpicyFoodList() {
       <ul>{foodList}</ul>
     </div>
   );
-}
+};
 
-export default SpicyFoodList;
+export default SpiceFoodList;
+
